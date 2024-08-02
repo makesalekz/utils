4ed8960c56f0403690b7f67be11a090a375a3558
Aidar Kudabaev <a.kudabaev@calendaria.ai>
2024-08-02 15:22:10 +0500

    added mock for jwt

--- Makefile
+++ Makefile
@@ -7,3 +7,5 @@ mock:
 	mockgen -source v1/nats/queues.go -destination v1/nats/mock/queues.go -package nats_mock
 	mockgen -source v2/dialer/dialer.go -destination v2/dialer/mock/dialer.go -package dialer_mock
 	mockgen -source v2/dialer/dialer_manager.go -destination v2/dialer/mock/dialer_manager.go -package dialer_mock
+	mockgen -source v2/jwt/claims.go -destination v2/jwt/mock/claims.go -package jwt_mock
+	mockgen -source v2/jwt/processor.go -destination v2/jwt/mock/processor.go -package jwt_mock
---
+++ v2/jwt/processor.go
@@ -0,0 +1,59 @@
+package jwt
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-kratos/kratos/v2/middleware/auth/jwt"
+	"gitlab.calendaria.team/services/utils/v1/config"
+	u_jwt "gitlab.calendaria.team/services/utils/v1/jwt"
+)
+
+type IJwtProcessor interface {
+	GetSecret() []byte
+	GetClaimsFromContext(ctx context.Context) (ITenantClaims, bool)
+	GetUserIdFromContext(ctx context.Context) int64
+}
+
+type JwtProcessor struct {
+	jwtSecret []byte
+}
+
+// NewJwtProcessor .
+func NewJwtProcessor(c *config.Config) (IJwtProcessor, error) {
+	secret, err := c.ReadGlobalSecretsFor(context.Background(), "jwt")
+	if err != nil {
+		return nil, fmt.Errorf("jwt secret not found, error: %w", err)
+	}
+
+	return &JwtProcessor{
+		jwtSecret: []byte(secret["data"].(string)),
+	}, nil
+}
+
+func (j *JwtProcessor) GetSecret() []byte {
+	return j.jwtSecret
+}
+
+func (j *JwtProcessor) GetClaimsFromContext(ctx context.Context) (ITenantClaims, bool) {
+	token, ok := jwt.FromContext(ctx)
+	if !ok {
+		return nil, false
+	}
+
+	claims, ok := token.(*u_jwt.TenantClaims)
+	if !ok {
+		return nil, false
+	}
+
+	return claims, true
+}
+
+func (j *JwtProcessor) GetUserIdFromContext(ctx context.Context) int64 {
+	claims, ok := j.GetClaimsFromContext(ctx)
+	if !ok || !claims.IsUserRequest() {
+		return 0
+	}
+
+	return claims.GetUserId()
+}
